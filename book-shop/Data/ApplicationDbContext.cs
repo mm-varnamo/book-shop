269daa2fbbd39f7424239a34a2b9df5643f9b338
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-05-16 22:58:55 +0200

    feat: add database connection

---
+++ book-shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace book_shop.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
