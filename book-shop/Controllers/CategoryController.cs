de220f9bafd0d9c6c26ad13cc19a2b12a8de733b
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-05-22 23:29:27 +0200

    feat: add category controller class and category view class

---
+++ book-shop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace book_shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
